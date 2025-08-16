ff9d6d8737a4c2a9db44f78b3179e5c31207ec49
harsh6468 <harsh.bhumca21@gmail.com>
2025-08-16 15:38:10 +0530

    Fix issues of columns not found

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -7,41 +7,38 @@ import os
 
 ENCODERS_PATH = os.path.join("models", "encoders.pkl")
 
-def preprocess_data(df, target_col=None, is_train=True):
-    # Extract lat/lon
-    if 'Landfill Location (Lat, Long)' in df.columns:
-        df[['Landfill Lat', 'Landfill Lon']] = df['Landfill Location (Lat, Long)'] \
-            .str.strip("()").str.split(",", expand=True).astype(float)
-        df.drop(columns=['Landfill Location (Lat, Long)'], inplace=True)
+def preprocess_data(df: pd.DataFrame, target_col=None, is_train=True):
+    encoders = joblib.load(ENCODERS_PATH)
 
-    # Drop unnecessary columns
-    if 'Landfill Name' in df.columns:
-        df.drop(columns=['Landfill Name'], inplace=True)
-
+    print(df['Waste Type'].values)
+    print(df['Disposal Method'].values)
+    for col in encoders["feature_names"]:
+        if df['Disposal Method'].values[0] in col or df['Waste Type'].values[0] in col:
+            df[col] = 1
+        elif col not in df.columns:
+            df[col] = 0
+        
     categorical_low = ['Waste Type', 'Disposal Method']
     categorical_high = ['City/District']
     numerical_cols = [
         'Waste Generated (Tons/Day)', 'Population Density (People/km²)',
         'Municipal Efficiency Score (1-10)', 'Cost of Waste Management (₹/Ton)',
-        'Awareness Campaigns Count', 'Landfill Capacity (Tons)', 'Year',
-        'Landfill Lat', 'Landfill Lon'
+        'Awareness Campaigns Count', 'Landfill Capacity (Tons)', 'Year'
     ]
 
-    df.fillna(df.mean(numeric_only=True), inplace=True)
+    df[categorical_high] = encoders["target_enc"].transform(df[categorical_high])
 
-    if is_train:
-        target_enc = TargetEncoder(cols=categorical_high)
-        df[categorical_high] = target_enc.fit_transform(df[categorical_high], df[target_col])
+    # One-hot encode
+    # df = pd.get_dummies(df, columns=categorical_low, drop_first=True)
 
-        scaler = StandardScaler()
-        df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
+    # Scale numerical
+    df[numerical_cols] = encoders["scaler"].transform(df[numerical_cols])
 
-        os.makedirs(os.path.dirname(ENCODERS_PATH), exist_ok=True)
-        joblib.dump({"target_enc": target_enc, "scaler": scaler}, ENCODERS_PATH)
-    else:
-        encoders = joblib.load(ENCODERS_PATH)
-        df[categorical_high] = encoders["target_enc"].transform(df[categorical_high])
-        df[numerical_cols] = encoders["scaler"].transform(df[numerical_cols])
-
-    df = pd.get_dummies(df, columns=categorical_low, drop_first=True)
+    # Ensure same columns as training
+    # for col in encoders["feature_names"]:
+    #     if col not in df.columns:
+    #         df[col] = 0
+    df = df[encoders["feature_names"]]
+    print(df.columns.tolist())
+    print(df.iloc[0].tolist())
     return df
